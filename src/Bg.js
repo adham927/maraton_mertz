918925751b56485a54a2993e1687848301d2e9c2
Adham <ad@gmail.com>
2024-05-01 21:41:14 +0300

    lesson 5

--- src/Bg.js
+++ src/Bg.js
@@ -1,14 +1,19 @@
 import './Bg.css';
 import close from './assets/close.png'
 import Download from './Download'
-import {useState} from "react"
+import {useState, useRef} from "react"
 import Disp_img from './Disp_img';
 import bunner from './assets/banner.png';
 import logo from './assets/logo.png';
 import Download_popup from './Download_popup';
+import axios from 'axios'
 
 function Bg() {
-
+  const inputElement = useRef();
+  const focusInput = () => {
+    inputElement.current.click();
+};
+  const [show_error, setShow_error] = useState(false);
   const [selected_tab, setselected_tab] = useState(true);
   const [show_eula, setShow_eula] = useState(false);
   const [show_download_popup, setShow_download_popup] = useState(false);
@@ -30,14 +35,40 @@ function Bg() {
     setShow_download_popup(!show_download_popup);
   }
 
+  function upload_file(e){
+    let file = e.target.files[0];
+    debugger;
+
+    if((file.type == 'image/png' || file.type == 'image/jpeg') && file.size <= 10000000){
+      let formData = new FormData();    
+
+      formData.append('fileImg', e.target.files[0]);
+      let headers={
+        'Content-Type': 'multipart/form-data'
+      }
+      axios.post('http://localhost:5000/upload_img',formData,headers)
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+    }
+    else{
+      setShow_error(true)
+    }
+  }
+
   return (
     <div>
       <div className="Bg_cont">
       <div className="header_title"> העלאת תמונה כדי להסיר את הרקע</div>
       <img className="close_img" src={close} alt="close"/>
 
-      <div className="upload_btn">העלאת תמונה </div>
+      <div className="upload_btn" onClick={focusInput}>העלאת תמונה </div>
       <div className='upload_btn_text' >פורמטים נתמכים jpeg, png</div>
+      {show_error ? <div className='err_img'>פורמט לא נתמך</div>: <></>}
+      <input type='file' onChange={upload_file} ref={inputElement} className='upload_input' />
       <div className="content_div">
         <div className="content_left">
             <div className="tabs_cont">
